1ddd3aebf09d36d6e8cacaba78cd41e8ebe2205e
Trajan <trajanmukalazi@gmail.com>
2020-03-18 16:46:31 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+console.log("hello world");
+
+/*------header section---*/
+
+var hello = "HELLO WORLD";
+$("#hello").text("HELLO WORLD");
+$("#contact").css("color","green");
+$("#contact").css("font-size","20px");
+
+if (hello == "HELLO WORLD") {
+    $("nav").css("background-color", "green" );
+} else {
+    console.log("it is right");
+}
